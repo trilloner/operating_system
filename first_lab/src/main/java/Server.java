edab96cfaf403f63457b6b09285463a7bdb4e878
Bogdan Volokhonenko <bo_10@ukr.net>
2020-09-19 11:16:53 +0300

    Update Server.java

--- first_lab/src/main/java/Server.java
+++ first_lab/src/main/java/Server.java
@@ -1,6 +1,23 @@
-
+import java.net.ServerSocket;
 
 public class Server {
+    private final int port;
+    private ServerSocket ss;
 
-    
+    public Server( int port) throws Exception {
+        this.port = port;
+        try {
+            ss = new ServerSocket(port);
+            System.out.println("Server created!");
+
+        }catch (Exception e){
+            System.out.println("Eror! Server not created");
+            throw e;
+
+        }
+    }
+
+    public void start(){
+
+    }
 }
