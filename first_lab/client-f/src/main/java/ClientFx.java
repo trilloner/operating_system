bcaa8f37ae407d6683a4f387fb740ba72cf61dab
Bogdan <bo_10@ukr.net>
2020-09-20 11:30:44 +0300

    Creating a function g(x)

--- first_lab/client-f/src/main/java/ClientFx.java
+++ first_lab/client-f/src/main/java/ClientFx.java
@@ -1,5 +1,7 @@
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ClientFx {
-    public int fx (int variant) throws InterruptedException {
+    public static int fx (int variant) throws InterruptedException {
         Thread.sleep(5);
         switch (variant){
             case 1:
@@ -10,16 +12,16 @@ public class ClientFx {
             case 3:
                 return 0;
             case 4:
+            case 6:
                 Thread.sleep(1000);
             case 5:
                 return (int) (2+ Math.random()*10);
-            case 6:
-                Thread.sleep(1000);
             default:
                 throw new IllegalArgumentException("Error!");
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
 
     }
 }
--- first_lab/client-g/src/main/java/ClientGx.java
+++ first_lab/client-g/src/main/java/ClientGx.java
@@ -1,2 +1,24 @@
 public class ClientGx {
+    public static int fx (int variant) throws InterruptedException {
+        Thread.sleep(5);
+        switch (variant){
+            case 1:
+                Thread.sleep(5);
+                return (int) (2+ Math.random()*10);
+            case 2:
+            case 6:
+                return (int) (2+ Math.random()*10);
+            case 3:
+            case 5:
+                Thread.sleep(5);
+            case 4:
+                return 0;
+            default:
+                throw new IllegalArgumentException("Error!");
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+
+
+    }
 }
