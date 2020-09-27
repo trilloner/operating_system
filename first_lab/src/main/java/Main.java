eda115aa140d817fa24e912e9d69f89ac6dbd06e
Bogdan Volokhonenko <bo_10@ukr.net>
2020-09-27 18:12:23 +0300

    testing

--- first_lab/src/main/java/Main.java
+++ first_lab/src/main/java/Main.java
@@ -2,8 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        Menu menu = new Menu();
-//        menu.menu();
-            new Server("localhost", 2020).start();
+            new Server("localhost", 2020, 1).run();
+
     }
 }
