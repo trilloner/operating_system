a05aa94182702772ed2ad31123fa20b7d23f36a8
Bogdan Volokhonenko <bo_10@ukr.net>
2020-09-27 23:04:39 +0300

    Updated Main

--- first_lab/src/main/java/Main.java
+++ first_lab/src/main/java/Main.java
@@ -1,12 +1,19 @@
+/**
+ * Operating system. Lab1. Variant 6
+ *
+ * @author Bogdan Volokhonenko
+ */
+
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-           Server mainServer =  new Server("localhost", 2020, 6,2);
-           mainServer.run();
-           System.out.println( mainServer.getResults());
-           System.out.println( mainServer.getMultiplication());
-           mainServer.close();
+
+
+    Menu menu = new Menu();
+    menu.menu();
+
 
     }
 }
