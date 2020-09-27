f36224f0dc7123e19ace54e537c48a6f6c6df4d5
Bogdan Volokhonenko <bo_10@ukr.net>
2020-09-27 20:57:45 +0300

    Testing

--- first_lab/src/main/java/Main.java
+++ first_lab/src/main/java/Main.java
@@ -2,7 +2,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-            new Server("localhost", 2020, 1).run();
+           Server mainServer =  new Server("localhost", 2020, 6,2);
+           mainServer.run();
+           System.out.println( mainServer.getResults());
+           System.out.println( mainServer.getMultiplication());
+           mainServer.close();
 
     }
 }
