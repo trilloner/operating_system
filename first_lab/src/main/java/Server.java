d88c124b822896b32e907ce5dfcf3f878613bd42
Bogdan Volokhonenko <bo_10@ukr.net>
2020-09-27 18:11:56 +0300

    Server updated

--- first_lab/src/main/java/Server.java
+++ first_lab/src/main/java/Server.java
@@ -1,14 +1,12 @@
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
+import java.util.List;
 
 public class Server {
     private Selector selector; //multiplexer
@@ -18,34 +16,53 @@ public class Server {
         this.address = new InetSocketAddress(host, port);
     }
 
-    public void start() throws IOException {
-        this.selector = Selector.open(); // init selector
-        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.bind(address);
-        serverSocketChannel.configureBlocking(false); // non blocking
-        serverSocketChannel.register(this.selector, SelectionKey.OP_ACCEPT); // register selector and type of event
+    public void run() {
+        try {
+            this.selector = Selector.open(); // init selector
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.bind(address);
+            serverSocketChannel.configureBlocking(false); // non blocking
+            serverSocketChannel.register(this.selector, SelectionKey.OP_ACCEPT); // register selector and type of event
 
-        System.out.println("Server started!");
+            new ClientGx().start();
+            new ClientFx().start();
+            System.out.println("Server started!");
 
-        while (true) {
-            //blocking wait for events
-            this.selector.select();
-            Iterator keys = this.selector.selectedKeys().iterator();
-            while (keys.hasNext()) {
-                SelectionKey key = (SelectionKey) keys.next();
-                keys.remove();
-                if (!key.isValid()) continue;
-                if (key.isAcceptable()) accept(key);
-                else if (key.isReadable()) read(key);
+            while (true) {
+                //blocking wait for events
+                this.selector.select();
+                Iterator keys = this.selector.selectedKeys().iterator();
+                while (keys.hasNext()) {
+                    SelectionKey key = (SelectionKey) keys.next();
+                    keys.remove();
+                    if (!key.isValid()) continue;
+                    if (key.isAcceptable()) accept(key);
+                    else if (key.isReadable()) read(key);
+                    //else if (key.isWritable()) write(key);
+                }
+
             }
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private void write(SelectionKey key) {
+        String s = "Connect to the server";
+        broadcast(s, key);
     }
 
     private void accept(SelectionKey key) throws IOException {
         ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
         SocketChannel channel = serverSocketChannel.accept(); // getting channel
         channel.configureBlocking(false);
+        channel.register(this.selector, SelectionKey.OP_WRITE);
+
+        String s = String.valueOf(this.variant);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        byteBuffer.put(s.getBytes());
+        byteBuffer.flip();
+        channel.write(byteBuffer);
         channel.register(this.selector, SelectionKey.OP_READ);
     }
 
@@ -63,7 +80,12 @@ public class Server {
         System.arraycopy(byteBuffer.array(), 0, data, 0, numRead);
         String gotData = new String(data);
         System.out.println("Got: " + gotData);
+
+        channel.register(this.selector, SelectionKey.OP_WRITE);
+
+
     }
+
 
     private void broadcast(String data, SelectionKey key) {
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
@@ -72,7 +94,7 @@ public class Server {
         try {
             SocketChannel channel = (SocketChannel) key.channel();
             channel.write(byteBuffer);
-            byteBuffer.flip();
+
 
         } catch (IOException e) {
             e.printStackTrace();
