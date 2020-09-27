97f29feacd9acc9a3656912ade2b57be27bb9001
Bogdan Volokhonenko <bo_10@ukr.net>
2020-09-27 23:04:47 +0300

    Added menu

--- first_lab/src/main/java/Menu.java
+++ first_lab/src/main/java/Menu.java
@@ -1,17 +1,34 @@
+/**
+ * Operating system. Lab1. Variant 6
+ *
+ * @author Bogdan Volokhonenko
+ */
 import java.util.Scanner;
 public class Menu {
-    public int menu(){
-        int select;
-        Scanner selector = new Scanner(System.in);
+    public void menu(){
+        while (true){
+            int select;
+            Scanner selector = new Scanner(System.in);
 
-        System.out.println("Testing: ");
-        System.out.println("1. f finishes before g with non value");
-        System.out.println("2. g finishes before f with non value");
-        System.out.println("3. f finishes before g with non value");
-        System.out.println("4. g finishes before f with non value");
+            System.out.println("Testing: ");
+            System.out.println("1. f finishes before g with non value");
+            System.out.println("2. g finishes before f with non value");
+            System.out.println("3. f finishes zero  g hangs");
+            System.out.println("4. g finishes zero f hangs");
+            System.out.println("5. f finishes non zero value f hangs");
+            System.out.println("6. g finishes non zero value f hangs");
 
-        select = selector.nextInt();
-        new ClientGx().start();
-        return select;
+            select = selector.nextInt();
+
+            if (select == 0){
+                System.exit(0);
+            }
+
+            Server mainServer =  new Server("localhost", 2020, select,2);
+            mainServer.run();
+            System.out.println( mainServer.getResults());
+            System.out.println( "RESULT OF MULTIPLY: " +  mainServer.getMultiplication() + "\n\n");
+            mainServer.close();
+        }
     }
 }
