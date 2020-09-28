69cfaa9864131df0f65fb9105891cbf2ca005d39
Bogdan Volokhonenko <bo_10@ukr.net>
2020-09-28 10:54:05 +0300

    added tests

--- first_lab/src/test/java/ServerTest.java
+++ first_lab/src/test/java/ServerTest.java
@@ -1,13 +1,49 @@
+/**
+ * Operating system. Lab1. Variant 6
+ *
+ * @author Bogdan Volokhonenko
+ */
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import spos.lab1.demo.IntOps;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Set;
+
 
 class ServerTest {
+
+
+    @Test
+    public void case1() throws InterruptedException {
+        Server mainServer =  new Server("localhost", 2020, 1,2);
+        mainServer.run();
+        HashMap<Integer,String> result = mainServer.getResults();
+        assertEquals(result.size(), 2);
+
+        assertEquals(result.keySet().toArray()[0],100);
+        assertEquals(result.keySet().toArray()[1],10);
+        assertEquals(5,IntOps.funcG(1));
+        assertEquals(3,IntOps.funcF(1));
+        assertEquals(mainServer.getMultiplication(), 1000);
+
+        mainServer.close();
+    }
+    @Test
+    public void case2() throws InterruptedException {
+        Server mainServer =  new Server("localhost", 2020, 2,2);
+        mainServer.run();
+        HashMap<Integer,String> result = mainServer.getResults();
+        assertEquals(result.size(), 2);
+
+
+        assertEquals(result.keySet().toArray()[0],20);
+        assertEquals(result.keySet().toArray()[1],10);
+
+        assertEquals(mainServer.getMultiplication(), 200);
+
+        mainServer.close();
+    }
 
     @Test
     public void case3() throws InterruptedException {
